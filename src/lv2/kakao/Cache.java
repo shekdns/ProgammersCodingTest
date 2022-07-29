3951b0410f0ea85744d3bf4f897cbb6cd76dfaeb
shekdns <shekdns>
2022-07-29 20:18:14 +0900

    프로그래머스 지도 캐시 카카오

---
+++ src/lv2/kakao/Cache.java
@@ -0,0 +1,51 @@
+package lv2.kakao;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Locale;
+
+public class Cache {
+  public static class Soltuion {
+    public static int CACHE_MISS = 5;
+    public static int CACHE_HIT  = 1;
+
+    public int solution( int cacheSize, String[] cities ) {
+      int answer = 0;
+
+      if( cacheSize == 0 ) {
+        answer = CACHE_MISS * cities.length;
+      } else {
+        Deque<String> queue = new LinkedList<>();
+
+        for( int i = 0; i < cities.length; i++ ) {
+          String city = cities[i].toLowerCase();
+
+          if( queue.remove( city ) == true ) {
+            queue.addFirst( city );
+            answer += CACHE_HIT;
+          } else {
+            int size = queue.size();
+            if( size == cacheSize ) {
+              queue.removeLast();
+            }
+            queue.addFirst( city );
+            answer += CACHE_MISS;
+          }
+        }
+      }
+      return answer;
+    }
+  }
+
+  public static void main(String[] args) {
+    Soltuion soltuion = new Soltuion();
+    int cacheSize    = 2;
+    String[] cities  = { "Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA" };
+    String[] cities1 = { "Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul" };
+    String[] cities2 = { "Jeju", "Pangyo", "NewYork", "newyork" };
+
+    int result = soltuion.solution( cacheSize, cities2 );
+
+    System.out.println( result );
+  }
+}
