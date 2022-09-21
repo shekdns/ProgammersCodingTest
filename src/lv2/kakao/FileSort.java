2577938d67e656739d36d730b59e9eaca3f4dab3
shekdns <aah03@naver.com>
2022-09-21 22:46:17 +0900

    파일정렬 클래스 및 Comparalbe 사용

--- src/lv2/kakao/FileSort.java
+++ src/lv2/kakao/FileSort.java
@@ -1,52 +1,96 @@
 package lv2.kakao;
 
-import java.util.Collections;
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
+import java.util.*;
 
 public class FileSort {
+  static class File implements Comparable<File>  {
+    int num;
+    String header = "";
+    String number = "";
+    String file_name = "";
+
+    File( String str ) {
+      String[] hArray = str.split( "\\d+" );
+      this.header = hArray[0];
+      String[] nArray = str.split( "\\D+" );
+      this.number = nArray[1];
+      this.file_name = str;
+    }
+
+    @Override
+    public int compareTo(File o) {
+      String str1 = this.header.toLowerCase(); // 자기 자신
+      String str2 = o.header.toLowerCase();    // 들어온 객체 비교
+
+      if( str1.equals( str2 ) ) {
+        int num1 = Integer.parseInt( this.number );
+        int num2 = Integer.parseInt( o.number );
+
+        if( num1 == num2 ) {
+          return this.num - o.num;
+        }
+        return num1 - num2;
+      }
+      return str1.compareTo( str2 );
+    }
+  }
   public static class Solution {
     public String[] solution( String[] files ) {
       String[] answer = {};
-      String[] header = {};
-      String[] number = {};
+
+      ArrayList<File> list = new ArrayList<>();
+
+      for( int i = 0; i < files.length; i++ ) {
+        list.add( new File( files[i] ) );
+      }
+
+      answer = new String[list.size()];
+
+      Collections.sort( list );
+
+      int i = 0;
+      for( File file : list ) {
+        answer[i++] = file.file_name;
+      }
 
       //1. 헤더 검사 2. 숫자 검사
-      for( int i = 0; i < files.length; i++ ) {
-        header = files[i].split( "\\d+" );  // 문자만 추출
-        //number = files[i].split( "\\D+" );  // 숫자만 추출
+//      for( int i = 0; i < files.length; i++ ) {
+//        header = files[i].split( "\\d+" );  // 문자만 추출
+//        //number = files[i].split( "\\D+" );  // 숫자만 추출
+//
+//        Character h = header[0].toLowerCase().charAt(0);
+//        for( int j = i + 1; j < files.length; j++ ) {
+//          String[] jHeader = files[j].split( "\\d+" );
+//          Character jh = jHeader[0].toLowerCase().charAt(0);
+//          if( h.compareTo( jh ) > 0 ) {
+//            String temp = files[i];
+//            files[i] = files[j];
+//            files[j] = temp;
+//          }
+//        }
+//      }
+//
+//      for( int i = 0; i < files.length; i++ ) {
+//        header = files[i].split( "\\d+" );  // 문자만 추출
+//        Character h = header[0].toLowerCase().charAt(0);
+//        for( int k = i + 1; k < files.length; k++ ) {
+//          number = files[i].split( "\\D+" );  // 숫자만 추출
+//
+//          String[] jHeader = files[k].split( "\\d+" );
+//          String[] kNumber = files[k].split( "\\D+" );
+//          Character jh = jHeader[0].toLowerCase().charAt(0);
+//          System.out.println( h + " " + jh );
+//          if( h.compareTo( jh ) == 0 ) {
+//            if( Integer.parseInt( String.valueOf( number[1] ) ) > Integer.parseInt( String.valueOf( kNumber[1]) ) ) {
+//              String temp = files[i];
+//              files[i] = files[k];
+//              files[k] = temp;
+//            }
+//          }
+//        }
+//      }
 
-        Character h = header[0].toLowerCase().charAt(0);
-        for( int j = i + 1; j < files.length; j++ ) {
-          String[] jHeader = files[j].split( "\\d+" );
-          Character jh = jHeader[0].toLowerCase().charAt(0);
-          if( h.compareTo( jh ) > 0 ) {
-            String temp = files[i];
-            files[i] = files[j];
-            files[j] = temp;
-          }
-        }
-      }
-
-      for( int i = 0; i < files.length; i++ ) {
-        header = files[i].split( "\\d+" );  // 문자만 추출
-        Character h = header[0].toLowerCase().charAt(0);
-        for( int k = i + 1; k < files.length; k++ ) {
-          number = files[i].split( "\\D+" );  // 숫자만 추출
-          String[] jHeader = files[k].split( "\\d+" );
-          String[] kNumber = files[k].split( "\\D+" );
-          Character jh = jHeader[0].toLowerCase().charAt(0);
-          if( h.compareTo( jh ) == 0 ) {
-            if( Integer.parseInt( String.valueOf( number[1] ) ) > Integer.parseInt( String.valueOf( kNumber[1]) ) ) {
-              String temp = files[i];
-              files[i] = files[k];
-              files[k] = temp;
-            }
-          }
-        }
-      }
-
-      answer = files;
+//      answer = files;
 
       return answer;
     }
@@ -73,6 +117,10 @@ public class FileSort {
     String testText2 = "foo010bar020.zip"; // Header foo Number 010 Tail zip
     String testText3 = "F-15";  // Header F- Number 15
 
+    String a = "img100.p2ng";
+    String b = "aa00001234563";
+    String c = "aa0000123456";
+
     String[] files4 = { testText1, testText2, testText3 };
 
     /*
@@ -80,7 +128,7 @@ public class FileSort {
 
      */
 
-    String[] result = solution.solution( files4 );
+    String[] result = solution.solution( files2 );
 
     for( String s : result ) {
       System.out.print( s + " " );
