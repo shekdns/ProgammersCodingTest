9b82b7b4b87bbdd6ec54deed5397d79dfbdba59b
shekdns <aah03@naver.com>
2022-09-23 19:14:49 +0900

    Jaden Case 풀이

--- src/lv2/JadenCase.java
+++ src/lv2/JadenCase.java
@@ -4,6 +4,27 @@ public class JadenCase {
   public static class Solution {
     public String solution( String s ) {
       String answer = "";
+      int index = 0;
+      s = s.toLowerCase();
+
+      StringBuilder sb = new StringBuilder();
+
+      for( int i = 0; i < s.length(); i++ ) {
+        Character ch = s.charAt(i);
+        String temp = ch.toString();
+        index++;
+        if( index == 1 ) {
+          temp = ch.toString().toUpperCase();
+          System.out.println( "CH = " + temp + " INDEX = " + index );
+        }
+        if( ch.toString().equals( " " ) ) {
+          index = 0;
+          System.out.println( "CCH = " + ch );
+        }
+        sb.append( temp );
+      }
+
+      answer = sb.toString();
 
       return answer;
     }
