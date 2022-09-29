fe4279b3665477bb4831e32b3b8d7a21f00783ac
shekdns <aah03@naver.com>
2022-09-29 21:56:40 +0900

    다리를 지나는 트럭

--- src/lv2/StackTruck.java
+++ src/lv2/StackTruck.java
@@ -2,48 +2,74 @@ package lv2;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 public class StackTruck {
   public static class Solution {
     public int solution( int bridge_length, int weight, int[] truck_weights ) {
-      int count = 0;
+      int answer = 0;
 
-      Queue<Integer> queue = new LinkedList<>();
 
-      int i = 0;
-      int qCount = 0;
-      while( i < truck_weights.length ) {
-        if (i == 0) {
-          queue.add(truck_weights[i]);
-          qCount++;
-          count++;
-          i++;
-          continue;
-        } else {
-          if( qCount == bridge_length ) {
-            queue.poll();
-            qCount = 1;
-            count++;
-            queue.add( truck_weights[i] );
-            i++;
-            continue;
+      Queue<Integer> bridgeQue = new LinkedList<>();
+      Queue<Integer> readyTruckQue = new LinkedList<>();
+
+      //초기 다리 상태 세팅
+
+      for(int i = 0 ; i<bridge_length ; i++)
+      {
+        bridgeQue.offer(0);
+      }
+      //System.out.println( bridgeQue );
+      //대기 트럭 세팅
+      for(int i : truck_weights)
+      {
+        readyTruckQue.offer(i);
+      }
+      //System.out.println( readyTruckQue );
+
+      int time = 0;
+      int totalWeight = 0;
+      while(!bridgeQue.isEmpty())
+      {
+        //while 한번 도는게 1초라고 생각
+        //시작하자마자 다리 맨 마지막부분에서 트럭을 빼버림
+        totalWeight-=bridgeQue.peek();
+        System.out.println( totalWeight );
+        bridgeQue.poll();
+        System.out.println( bridgeQue );
+
+        //대기 트럭열에 있던 마지막 트럭이 다리에 올라가면, 다리 길이 만큼의 시간 후에
+        //마지막 트럭이 다리를 건너기때문에 다리 길이(*1초) 만큼 시간이 소요되면 모든 트럭이 건너게됨
+        if(readyTruckQue.isEmpty())
+        {
+          time+=bridge_length;
+          break;
+        }
+        else
+        {
+
+          //다리의 하중량을 버틸 수 있으면 트럭 투입
+          if((totalWeight+readyTruckQue.peek()) <= weight)
+          {
+            bridgeQue.offer(readyTruckQue.peek());
+            totalWeight+=readyTruckQue.peek();
+            readyTruckQue.poll();
           }
-          int qp = queue.peek() == null ? 0 : queue.peek();
-          System.out.println( "QP = " + qp );
-          int w = weight - qp;
-          if( truck_weights[i] > w ) {
-            qCount++;
-            count++;
-            continue;
-          } else {
-            queue.add( truck_weights[i] );
-            qCount++;
-            count++;
-            continue;
+          //다리의 하중량때문에 트럭 못들어가면 0으로 넣어줌
+          else {
+            bridgeQue.offer(0);
           }
         }
+
+        time++;
+
       }
-      return count;
+
+      answer = time;
+
+
+
+      return answer;
     }
   }
 
@@ -56,9 +82,10 @@ public class StackTruck {
     int a = 100;
     int b = 100;
     int[] c = { 10 };
+    int[] cc = { 10,10,10,10,10,10,10,10,10,10 };
 
-    //int result = solution.solution( bridge_length, weight, truck_weights );
-    int result = solution.solution( a, b, c );
+    int result = solution.solution( bridge_length, weight, truck_weights );
+    //int result = solution.solution( a, b, cc );
     System.out.println( "RESULT = " + result );
   }
 }
