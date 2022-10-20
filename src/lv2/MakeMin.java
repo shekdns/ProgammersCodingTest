1c410e4aec2d558c5707bdd7751017bab170c6f1
shekdns <aah03@naver.com>
2022-10-20 23:01:46 +0900

    최솟값 만들기

---
+++ src/lv2/MakeMin.java
@@ -0,0 +1,20 @@
+package lv2;
+
+public class MakeMin {
+  public static class Solution {
+    public int solution( int[] A, int[] B ) {
+      int answer = 0;
+
+      return answer;
+    }
+  }
+
+  public static void main(String[] args) {
+    Solution solution = new Solution();
+    int[] a = { 1, 4, 2 };
+    int[] b = { 5, 4, 4 };
+
+    int result = solution.solution( a, b );
+    System.out.println( "RESULT = " + result );
+  }
+}
