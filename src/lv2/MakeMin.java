8216632cb47426f089696f35c550ef932a044dca
shekdns <aah03@naver.com>
2022-10-21 19:11:20 +0900

    최솟값 구하기

--- src/lv2/MakeMin.java
+++ src/lv2/MakeMin.java
@@ -1,9 +1,24 @@
 package lv2;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class MakeMin {
   public static class Solution {
     public int solution( int[] A, int[] B ) {
       int answer = 0;
+      Integer[] BB = new Integer[B.length];
+      for( int i = 0; i < B.length; i++ ) {
+        BB[i] = B[i];
+      }
+
+      Arrays.sort( A );
+      Arrays.sort( BB, Collections.reverseOrder() );
+
+      for( int i = 0; i < A.length; i++ ) {
+        answer += ( A[i] * BB[i] );
+      }
 
       return answer;
     }
