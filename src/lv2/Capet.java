213a741ba20f66525d35681d0d3848fc5fa3c3f5
shekdns <aah03@naver.com>
2022-10-25 21:43:01 +0900

    카펫

--- src/lv2/Capet.java
+++ src/lv2/Capet.java
@@ -14,19 +14,52 @@ public class Capet {
           arrayList.add( i );
         }
       }
+
+      if( arrayList.size() % 2 == 0 ) {
+        for( int i = 0; i < arrayList.size(); i++ ) {
+          if( (arrayList.get(i) * arrayList.get( arrayList.size() - 1 - i )) == temp ) {
+            if( ( arrayList.get( arrayList.size() - 1 - i ) - 2 ) * ( arrayList.get( i ) - 2 ) == yellow  ) {
+              answer[0] = arrayList.get( arrayList.size() - 1 - i );
+              answer[1] = arrayList.get(i);
+              break;
+            }
+          }
+        }
+      } else {
+        int l = arrayList.size() / 2;
+        answer[0] = arrayList.get(l);
+        answer[1] = arrayList.get(l);
+//        for( int i = 0; i < arrayList.size(); i++ ) {
+//          if( yellow + 2 == arrayList.get(i)  ) {
+//            answer[0] = arrayList.get(i);
+//            answer[1] = arrayList.get(i);
+//            break;
+//          }
+//        }
+      }
+
+//      for( int i = 0; i < arrayList.size(); i++ ) {
+        //System.out.println( "앞 = " + arrayList.get(i) );
+        //System.out.println( "뒤 = " + arrayList.get( arrayList.size() - 1 - i ));
+//        if( (arrayList.get(i) * arrayList.get( arrayList.size() - 1 - i )) == temp ) {
+//          answer[0] = arrayList.get( arrayList.size() - 1 - i );
+//          answer[1] = arrayList.get(i);
+//          break;
+//        }
+//      }
       //12 -> 1 2 3 4 6 12
       //48 -> 1 2 3 4 6 8 12 16 24 48
       // 9 -> 1 3 9
       //24 -> 1 2 3 4 6 8 12 24
-
+      //6  -> 1 2 3 6
       return answer;
     }
   }
 
   public static void main(String[] args) {
     Solution solution = new Solution();
-    int brown  = 18;   // 24 24  -> 8 6    1 2 3 4 6 8 12 24
-    int yellow = 6;   // -> 4 3   -> 1 2
+    int brown  = 8;   // 24 24  -> 8 6    1 2 3 4 6 8 12 24
+    int yellow = 1;   // -> 4 3   -> 1 2
 
     int[] result = solution.solution( brown, yellow );
 
