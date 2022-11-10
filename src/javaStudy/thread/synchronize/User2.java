a9f2ca4e8c52133fa6040f2ab27d509e3edac18d
shekdns <aah03@naver.com>
2022-11-11 01:12:45 +0900

    쓰레드 공부

---
+++ src/javaStudy/thread/synchronize/User2.java
@@ -0,0 +1,16 @@
+package javaStudy.thread.synchronize;
+
+//User2 스레드
+public class User2 extends Thread {
+  private Calculator calculator;
+
+  public void setCalculator( Calculator calculator ){
+    this.setName( "User2" );
+    this.calculator = calculator;
+  }
+
+  @Override
+  public void run() {
+    calculator.setMemory( 50 );
+  }
+}
