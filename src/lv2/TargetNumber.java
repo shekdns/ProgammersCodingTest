2783983bb81e0ba9bc517db6ffaabcc47fd31379
shekdns <aah03@naver.com>
2022-12-08 23:07:44 +0900

    깊이,너비 우선탐색 DFS,BFS 타겟넘버

---
+++ src/lv2/TargetNumber.java
@@ -0,0 +1,22 @@
+package lv2;
+
+//깊이,너비 우선탐색 DFS,BFS 타겟넘버
+public class TargetNumber {
+  public static class Solution {
+
+    public int solution( int[] numbers, int target ) {
+      int answer = 0;
+
+      return answer;
+    }
+  }
+
+  public static void main(String[] args) {
+    Solution solution = new Solution();
+    int[] numbers = { 1, 1, 1, 1, 1 };
+    int target = 3;
+
+    int result = solution.solution( numbers, target );
+    System.out.println( "RESULT = " + result );
+  }
+}
