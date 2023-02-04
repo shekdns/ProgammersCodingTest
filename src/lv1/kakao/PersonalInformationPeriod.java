9f0c6f3aa415cae2b00ccecc796808e2c7080aed
shekdns <aah03@naver.com>
2023-02-05 00:38:04 +0900

    2023 KAKAO BLIND RECRUITMENT 개인정보 수집 유효기간 풀이성공

--- src/lv1/kakao/PersonalInformationPeriod.java
+++ src/lv1/kakao/PersonalInformationPeriod.java
@@ -12,16 +12,17 @@ public class PersonalInformationPeriod {
   private static class Solution {
     // 모두 -1일식 해야됨 그래서 1월 1일자의 경우 month도 - 해야됨  2021.01.01 -> 2021.12.28, 2021.12.28 -> 2022.11.27
     public static String returnAccesDay( String[] array, int access ) {
-      int year  = Integer.parseInt( array[0] );
-      int month = Integer.parseInt( array[1] );
-      int day   = ( Integer.parseInt( array[2] ) ) == 1 ? 28 : Integer.parseInt( array[2] ) - 1;
+      int year  = Integer.parseInt( array[0] ); // 2021
+      int month = Integer.parseInt( array[1] ); // 01
+      int day   = ( Integer.parseInt( array[2] ) == 1 ) ? 28 : Integer.parseInt( array[2] ) - 1; // 28
 
       month = ( day == 28 ) ? month - 1 : month;
-      year  = ( month == 1 && day == 1 ) ? year - 1 : year;
 
       int d = month + access;
+
       int mok = d / 12;
       int na  = d % 12;
+
       if( mok != 0 && na == 0 ) {
         mok = mok - 1;
         na  = 12;
@@ -46,7 +47,12 @@ public class PersonalInformationPeriod {
         String[] split = privacies[i].split( " " );
         String[] array = split[0].split( "[.]" );
         int access = map.get( split[1] );
-        String accessDay = returnAccesDay( array, access );
+        String accessDay = "";
+        if( access == 0 ) {
+          accessDay = privacies[i];
+        } else {
+          accessDay = returnAccesDay( array, access );
+        };
         System.out.println( "AA = " + accessDay );
 //        if( Integer.parseInt( today.replace( ".", "" ) ) > Integer.parseInt( accessDay.replace( ".", "" ) ) ) {
 //          arrayList.add( i + 1 );
@@ -62,8 +68,8 @@ public class PersonalInformationPeriod {
   public static void main(String[] args) {
     Solution solution = new Solution();
     String today1 = "2022.05.19";
-    String[] terms1 = { "A 6", "B 12", "C 3" };
-    String[] privacies1 = { "2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C", "2021.01.01 B", "2021.12.28 B" };
+    String[] terms1 = { "A 6", "B 12", "C 3", "D 13" };
+    String[] privacies1 = { "2020.01.01 B", "2022.01.02 B" };
 
     String today2 = "2020.01.01";
     String[] terms2 = {"Z 3", "D 5"};
