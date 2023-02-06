bc2c54b196c4b3fdecdd5c9134df5977a4bda1e5
shekdns <aah03@naver.com>
2023-02-06 23:16:03 +0900

    타켓넘버 DFS 풀이

--- src/lv2/TargetNumber.java
+++ src/lv2/TargetNumber.java
@@ -3,11 +3,23 @@ package lv2;
 //깊이,너비 우선탐색 DFS,BFS 타겟넘버
 public class TargetNumber {
   public static class Solution {
-
+    static int answer;
     public int solution( int[] numbers, int target ) {
-      int answer = 0;
+      answer = 0;
+      dfs( 0, 0, numbers, target );
 
       return answer;
+    }
+    public void dfs( int n, int sum, int[] numbers, int target ) {
+      if( n == numbers.length ) {
+        if( sum == target ) {
+          answer++;
+        }
+        return;
+      }
+
+      dfs( n + 1, sum + numbers[n], numbers, target );
+      dfs( n + 1, sum - numbers[n], numbers, target );
     }
   }
 
