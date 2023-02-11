fe011aeaced7974630c6467694177d378b48a9bb
shekdns <aah03@naver.com>
2023-02-11 21:07:06 +0900

    2018 KAKAO BLIND N진수게임 통과

--- src/lv2/kakao/NGAME.java
+++ src/lv2/kakao/NGAME.java
@@ -7,44 +7,57 @@ public class NGAME {
 
   public static class Solution {
     public String solution(int n, int t, int m, int p) {
+      //String[] array = { "A", "B", "C", "D", "E", "F" };
       String answer = ""; //Integer.toString( i, m );
-
+//13579BDF01234567
       StringBuilder sb = new StringBuilder();
-      for( int i = 0; i <= t * 2; i ++ ) {
+      for( int i = 0; i <= t * m; i ++ ) {
         String temp = Integer.toString( i, n );
+        System.out.println( "i = " + i  + " temp = " + temp.toUpperCase() );
         sb.append( temp.toUpperCase() );
       }
       System.out.println( sb );
-      LinkedList<String> list = new LinkedList<>();
+      StringBuilder sb2 = new StringBuilder();
       int count = 0;
-      for( int i = 0; i < String.valueOf( sb ).length(); i++ ) {
+      int i = 0;
+      while( true ) {
         count++;
         Character ch = String.valueOf( sb ).charAt( i );
-        list.add( count + ":" + ch );
+        i++;
+        if( count == p ) {
+          sb2.append( ch );
+        }
+        if( sb2.length() == t ) {
+          break;
+        }
         if( count == m ) {
           count = 0;
         }
       }
-      System.out.println( list );
-      StringBuilder tt = new StringBuilder();
-      for( String s : list ) {
-        String[] split = s.split( ":" );
-        if( split[0].equals( String.valueOf( p ) ) ) {
-          tt.append( split[1] );
-        }
-      }
-
-      System.out.println( tt.substring( 0 , t ) );
-      return answer;
+//      for( int i = 0; i < String.valueOf( sb ).length(); i++ ) {
+//        count++;
+//        Character ch = String.valueOf( sb ).charAt( i );
+//        if( count == p ) {
+//          sb2.append( ch );
+//        }
+//        if( sb2.length() == t ) {
+//          break;
+//        }
+//        if( count == m ) {
+//          count = 0;
+//        }
+//      }
+      System.out.println( sb2 );
+      return sb2.toString();
     }
   }
 
   public static void main(String[] args) {
     Solution solution = new Solution();
-    int n = 16; //진수
-    int t = 16; //구해야할 값
+    int n = 2; //진수
+    int t = 4; //구해야할 값
     int m = 2; //인원
-    int p = 2; //순번
+    int p = 1; //순번
     //0111
     String result = solution.solution( n, t, m, p );
     System.out.println( "RESULT = " + result );
