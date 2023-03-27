6a8646fe3061464cc81bbaf00c179f45204b1e70
shekdns <aah03@naver.com>
2023-03-27 22:23:43 +0900

    택배상자

---
+++ src/lv2/DeliveryBox.java
@@ -0,0 +1,18 @@
+package lv2;
+//택배 상자
+public class DeliveryBox {
+  public static class Solution {
+    public int solution( int[] order ) {
+      int answer = 0;
+
+      return answer;
+    }
+  }
+
+  public static void main(String[] args) {
+    Solution solution = new Solution();
+    int[] order = { 4,3,1,2,5 };
+    int result = solution.solution( order );
+    System.out.println( "RESULT = " + result );
+  }
+}
