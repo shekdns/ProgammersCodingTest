b4cc2b3d0502fb3fc3918288fc3e411af1ede55e
shekdns <aah03@naver.com>
2023-03-31 22:52:32 +0900

    택배상자 풀이완료

--- src/lv2/DeliveryBox.java
+++ src/lv2/DeliveryBox.java
@@ -1,9 +1,32 @@
 package lv2;
+
+import java.util.Stack;
+
 //택배 상자
 public class DeliveryBox {
   public static class Solution {
     public int solution( int[] order ) {
       int answer = 0;
+
+      Stack<Integer> Box = new Stack<>();
+
+      int i = 0;
+      int j = 0;
+      while( i < order.length ) {
+        Box.push( i + 1) ;
+
+        while( !Box.isEmpty() ) {
+          int item = Box.peek();
+          if( item == order[j] ) {
+            j++;
+            Box.pop();
+            answer++;
+          } else {
+            break;
+          }
+        }
+        i++;
+      }
 
       return answer;
     }
@@ -11,7 +34,7 @@ public class DeliveryBox {
 
   public static void main(String[] args) {
     Solution solution = new Solution();
-    int[] order = { 4,3,1,2,5 };
+    int[] order = { 5,4,3,2,1 };
     int result = solution.solution( order );
     System.out.println( "RESULT = " + result );
   }
