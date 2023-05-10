699bf5eb9e93793ed48f39d48d7d863a116e66b6
shekdns <aah03@naver.com>
2023-05-10 23:30:26 +0900

    2018 KAKAO BLIND 그곡명

--- src/lv2/kakao/TheSong.java
+++ src/lv2/kakao/TheSong.java
@@ -1,9 +1,30 @@
 package lv2.kakao;
+
+import java.util.Stack;
+
 //방금그곡
 public class TheSong {
   public static class Solution{
     public String solution( String m, String[] musicinfos ) {
       String answer = "";
+
+      Stack<String> stack = new Stack<>();
+      /*
+      1. 시작 시간 부터 종료 시간 까지 1분당 멜로디가 1개식 추가
+      2. 시작 멜로디는 melodie 멜로디 부터 시작
+      3.
+       */
+      for( int i = 0; i < musicinfos.length; i++ ) {
+        String[] split = musicinfos[i].split( "," );
+        String startTime = split[0].replace( ":", "" );
+        String endTime   = split[1].replace( ":", "" );
+        String title     = split[2];
+        String melodie   = split[3];
+        int melodieSize  = melodie.length();
+        int dir = Integer.parseInt( startTime ) - Integer.parseInt( endTime );
+
+        String fullMelodie = melodie + m;
+      }
 
       return answer;
     }
@@ -14,6 +35,9 @@ public class TheSong {
     String[] musicinfos = { "12:00,12:14,HELLO,CDEFGAB", "13:00,13:05,WORLD,ABCDEF" };
     Solution solution = new Solution();
 
+    /*
+
+     */
     System.out.println( "RESULT = " + solution.solution( m, musicinfos ) );
   }
 }
