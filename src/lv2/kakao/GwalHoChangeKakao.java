8acf59be58ce690a701b26a98a6aad34030a2cdc
shekdns <aah03@naver.com>
2023-05-22 23:14:48 +0900

    2020 KAKAO BLIND RECRUITMNET 괄호 변환

---
+++ src/lv2/kakao/GwalHoChangeKakao.java
@@ -0,0 +1,21 @@
+package lv2.kakao;
+//2020 KAKAO BLIND RECRUITMNET 괄호 변환
+public class GwalHoChangeKakao {
+
+  public static class Solution {
+    public String solution( String p ) {
+      String answer = "";
+
+      return answer;
+    }
+  }
+
+  public static void main(String[] args) {
+    Solution solution = new Solution();
+    String p1 = "(()())()";
+    String p2 = ")(";
+    String p3 = "()))((()";
+
+    System.out.println( solution.solution( p1 ) );
+  }
+}
