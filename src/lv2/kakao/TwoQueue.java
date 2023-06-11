f8e8d765c9d71f23dae1ee0f72fc4241b90495ed
shekdns <aah03@naver.com>
2023-06-11 21:21:58 +0900

    2022 KAKAO TECH INTERNSHIP 두 큐 합 같게 만들기

---
+++ src/lv2/kakao/TwoQueue.java
@@ -0,0 +1,21 @@
+package lv2.kakao;
+
+//2022 KAKAO TECH INTERNSHIP 두 큐 합 같게 만들기
+public class TwoQueue {
+
+  public static class Solution {
+    public int solution( int[] queue1, int[] queue2 ) {
+      int answer = -2;
+
+      return answer;
+    }
+  }
+
+  public static void main(String[] args) {
+    Solution solution = new Solution();
+    int[] queue1 = { 3, 2, 7, 2 };
+    int[] queue2 = { 4, 6, 5, 1 };
+
+    System.out.println( solution.solution( queue1, queue2 ) );
+  }
+}
