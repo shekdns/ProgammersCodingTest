2c2c29a706b93400e9861799cd19e840484e1171
shekdns <aah03@naver.com>
2023-06-26 15:16:51 +0900

    무인도 여행

--- src/lv2/IslandTrip.java
+++ src/lv2/IslandTrip.java
@@ -2,8 +2,29 @@ package lv2;
 //무인드 여행
 public class IslandTrip {
   public static class Solution {
+    public static int[][] distance = { {-1,0}, {1,0}, {0,-1}, {0,1} };
+    static String[][] Map = {};
+
+    public int[] dfs( int[] answer ) {
+
+      return answer;
+    }
+
     public int[] solution(String[] maps) {
       int[] answer = {};
+
+      Map = new String[maps.length][maps[0].length()];
+
+      for( int i = 0; i < maps.length; i++ ) {
+        String temp = maps[i];
+        for( int j = 0; j < temp.length(); j++ ) {
+          Character ch = temp.charAt( j );
+          Map[i][j] = String.valueOf( ch );
+        }
+      }
+
+
+
       return answer;
     }
   }
