fad66d2e56f3fe247f77a58eccdd26bfeb8f0fb6
shekdns <aah03@naver.com>
2023-08-06 11:58:31 +0900

    피로도

---
+++ src/Pirodo.java
@@ -0,0 +1,18 @@
+//피로도
+public class Pirodo {
+  public static class Solution {
+    public int solution( int k, int[][] dungeons ) {
+      int answer = -1;
+
+      return answer;
+    }
+  }
+
+  public static void main(String[] args) {
+    Solution solution = new Solution();
+    int k = 80;
+    int[][] dungeons = { { 80, 20 }, { 50, 40 }, { 30, 10 } };
+
+    System.out.println( "RESULT = " + solution.solution( k, dungeons ) );
+  }
+}
