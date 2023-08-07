6216d9f15c645af5998cdef1a85fbd4336a2cf95
shekdns <aah03@naver.com>
2023-08-07 20:17:26 +0900

    피로도 100% 풀이완료

--- src/Pirodo.java
+++ src/Pirodo.java
@@ -1,21 +1,24 @@
-import java.util.ArrayList;
 import java.util.Collections;
 
 //피로도
 public class Pirodo {
   public static class Solution {
-    ArrayList<Integer> arrayList = new ArrayList<>();
+    int max = -1;
     public void permutation( int minP[], int[] useP, int[] outputM, int[] outputU, boolean[] visited, int depth, int r, int k ) {
       if( depth == r ) {
         int tempK = k;
         int count = 0;
         for( int i = 0; i < r; i++ ) {
+          System.out.print( outputM[i] + " " );
           if( tempK >= outputM[i] ) {
             tempK -= outputU[i];
             count++;
           }
         }
-        arrayList.add( count );
+        System.out.println();
+        if( max < count ) {
+          max = count;
+        }
         return;
       }
       for( int i = 0; i < minP.length; i++ ) {
@@ -23,7 +26,7 @@ public class Pirodo {
           visited[i] = true;
           outputM[depth] = minP[i];
           outputU[depth] = useP[i];
-          permutation( minP, useP, outputM, outputU,visited, depth + 1, 3, k );
+          permutation( minP, useP, outputM, outputU,visited, depth + 1, minP.length, k );
           visited[i] = false;
         }
       }
@@ -47,11 +50,8 @@ public class Pirodo {
         }
       }
 
-      permutation( minP, useP, outputM, outputU,visited, 0, 3, k );
-      Collections.sort( arrayList, Collections.reverseOrder() );
-
-      answer = arrayList.get(0);
-
+      permutation( minP, useP, outputM, outputU,visited, 0, dungeons.length, k );
+      answer = max;
       return answer;
     }
   }
@@ -61,6 +61,8 @@ public class Pirodo {
     int k = 80;
     int[][] dungeons = { { 80, 20 }, { 50, 40 }, { 30, 10 } };
 
-    System.out.println( "RESULT = " + solution.solution( k, dungeons ) );
+    int k2 = 10;
+    int[][] dungeons2 = { { 9, 2 }, { 10, 3 }, { 7, 3 }, { 5, 4 }, { 1, 1 }  };
+    System.out.println( "RESULT = " + solution.solution( k2, dungeons2 ) );
   }
 }
