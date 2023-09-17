f326df85d0121f38c8c14bb6b902328d85e49650
shekdns <aah03@naver.com>
2023-09-17 14:50:21 +0900

    전력망 둘로나누기

--- src/lv2/DivideTheGridInTwo.java
+++ src/lv2/DivideTheGridInTwo.java
@@ -1,32 +1,54 @@
 package lv2;
 
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
+import java.util.*;
 
 //전력망을 둘로 나누기(완전탐색)
 public class DivideTheGridInTwo {
   public static class Solution {
-    public int solution( int n, int[][] wires ) {
-      int answer = -1;
 
-      LinkedHashMap<Integer,LinkedList<Integer>> lMap = new LinkedHashMap<>();
-      for( int i = 1; i <= n; i++ ) {
-        LinkedList<Integer> list = new LinkedList<>();
-        for( int j = 0; j< wires.length; j++ ) {
-          int startPoint = wires[j][0];
-          int endPoint   = wires[j][1];
-          if( startPoint == i ) {
-            list.add( endPoint );
-          }
-          if( endPoint == i ) {
-            list.add( startPoint );
-          }
-        }
-        lMap.put( i, list );
+    public int getBfsNum( int index, int[][] wires ) {
+      int count = 0;
+
+      List<int[]> list = new ArrayList<>();
+      for( int[] temp : wires ) {
+        list.add( temp );
       }
 
-      System.out.println( lMap );
+      Queue<Integer> queue = new LinkedList<>();
+      queue.add( list.get( index )[0] );
+      list.remove( index );
 
+      while( !queue.isEmpty() ) {
+        int check = queue.remove();
+        for( int i = 0; i < list.size(); i++ ) {
+          int[] array = list.get(i);
+          if( check == array[1] ) {
+            queue.add( list.get(i)[0] );
+            list.remove( array );
+            i--;
+          }
+          if( check == array[0] ) {
+            queue.add( list.get(i)[1] );
+            list.remove( array );
+            i--;
+          }
+        }
+        count++;
+      }
+      return count;
+    }
+
+    public int solution( int n, int[][] wires ) {
+      int answer = Integer.MAX_VALUE;
+      int num1   = 0;
+      int num2   = 0;
+
+      for( int i = 0; i < wires.length; i++ ) {
+        num1 = getBfsNum( i, wires );
+        num2 = n - num1;
+        int temp = Math.abs( num1 - num2 );
+        answer = Math.min( temp, answer );
+      }
       return answer;
     }
   }
@@ -39,6 +61,6 @@ public class DivideTheGridInTwo {
     int[][] wires1 = { { 1, 3 }, { 2, 3 }, { 3, 4 }, { 4, 5 }, { 4, 6 }, { 4, 7 }, { 7, 8 }, { 7, 9 }  };
     int[][] wires2 = { { 1, 2 }, { 2, 3 }, { 3, 4 } };
     int[][] wires3 = { { 1, 2 }, { 2, 7 }, { 3, 7 }, { 3, 4 }, { 4, 5 }, { 6, 7 } };
-    System.out.println( "RESULT = " + solution.solution( n3, wires3 ) );
+    System.out.println( "RESULT = " + solution.solution( n2, wires2 ) );
   }
 }
