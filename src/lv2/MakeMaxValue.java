44c0f88c0393be81ffae50b72a21a967a6a0a8f0
shekdns <aah03@naver.com>
2023-11-12 20:29:32 +0900

    큰 수 만들기

---
+++ src/lv2/MakeMaxValue.java
@@ -0,0 +1,23 @@
+package lv2;
+//큰 수 만들기
+public class MakeMaxValue {
+  public static class Solution {
+    public String solution( String number, int k ) {
+      String answer = "";
+
+
+
+      return answer;
+    }
+  }
+  public static void main(String[] args) {
+    Solution solution = new Solution();
+    String number1 = "1924";
+    String number2 = "1231234";
+
+    int k1 = 2;
+    int k2 = 3;
+
+    System.out.println( "RESULT = " + solution.solution( number1, k1 ) );
+  }
+}
