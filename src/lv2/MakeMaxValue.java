6ab38d397919f1ea55a7f7bb7659be52ffacfe2a
shekdns <aah03@naver.com>
2023-11-14 21:54:27 +0900

    큰수 만들기 (진행중)

--- src/lv2/MakeMaxValue.java
+++ src/lv2/MakeMaxValue.java
@@ -5,7 +5,28 @@ public class MakeMaxValue {
     public String solution( String number, int k ) {
       String answer = "";
 
+      String front = number.substring( 0, number.length() - k  );
+      String back  = number.substring( number.length() - k  );
+      System.out.println( "F = " + front );
+      int postion = 0;
+      int leng = front.length() - k;
+      System.out.println( leng );
+      Character ch = front.charAt( 0 );
+      int max = Integer.parseInt( ch.toString() );
 
+      if( front.length() + 1 <= k ) {
+
+      }
+//      for( int i = 1; i < front.length(); i++ ) {
+//        postion = i;
+//        ch = front.charAt( i );
+//        int t = Integer.parseInt( ch.toString( ) );
+//        if(  )
+//
+//      }
+      //417725
+      String d = "417725";
+      System.out.println( d.substring( d.length() - ( d.length() - 5 ) ) );
 
       return answer;
     }
@@ -14,10 +35,12 @@ public class MakeMaxValue {
     Solution solution = new Solution();
     String number1 = "1924";
     String number2 = "1231234";
+    String number3 = "4177252841";
 
     int k1 = 2;
     int k2 = 3;
+    int k3 = 4;
 
-    System.out.println( "RESULT = " + solution.solution( number1, k1 ) );
+    System.out.println( "RESULT = " + solution.solution( number3, k3 ) );
   }
 }
